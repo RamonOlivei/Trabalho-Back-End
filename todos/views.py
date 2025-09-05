1800e21086938252b3bc863272f7818aaff3e84e
Ramon Oliveira <ramoncostaoliveira99@gmail.com>
2025-09-04 21:04:18 -0300

    criacao da app: Read

---
+++ todos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic import ListView
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
+    template_name = "todos/todo_list.html"  # Local do template
+    context_object_name = "todos"           # Nome da lista no template
